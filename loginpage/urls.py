398cb1a15965c153ef2c95690e10e4a01621d131
Karthick Ramanathan <karthick.ramanathan96@gmail.com>
2021-02-23 19:31:29 +0530

    CBV for username password created

--- loginpage/urls.py
+++ loginpage/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import login_info_collect
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('',views.index,name='index'),
+    path('login_info/',login_info_collect.as_view(),name='login_info')
 ]
--- loginpage/views.py
+++ loginpage/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
 def index(request):
     return render(request,'loginpage/login.html') 
+
+class login_info_collect(View):
+    def post(self,request):
+        user_name=request.POST.get('user_name')
+        password=request.POST.get('user_password')
+        print('username: ',user_name,'password:',password)
+        return HttpResponse("Succeed")
+    
